82ce7ef2dda927bd4d16af3f7e657b5d276d21a0
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-09-24 20:31:01 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-resource "dynatrace_autotag_v2" "sampleagremote" {
-  name                        = "sampleagremote"
-  /* ensure that this resource instance doesn't maintain the rules */
-  rules_maintained_externally = true
-}
-
-/*
-  Be careful when maintaining `dynatrace_autotag_rules` in separate modules.
-  Do not execute `terraform apply` in parallel when several modules contain 
-  `dynatrace_autotag_rules` referring to the same `dynatrace_autotag_v2`.
-*/
-resource "dynatrace_autotag_rules" "sampleagremote" {
-  auto_tag_id = dynatrace_autotag_v2.sampleagremote.id
-  rules {
-    rule {
-      type                = "SELECTOR"
-      enabled             = true
-      entity_selector     = "type(SERVICE),tag(sampleagremote)"
-      value_format        = "disabled"
-      value_normalization = "Leave text as-is"
-    }
-  }
+module "autotag_v2" {
+  source = "./modules/autotag_v2"
 }
