78105425e65942a55c92293872593921d2d7cbdc
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-09-28 20:22:38 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "dynatrace_autotag_v2" "crossplane-created" {
-  name                          = "crossplane-created"
+resource "dynatrace_autotag_v2" "crossplane-created2" {
+  name                          = "crossplane-created2"
   description                   = "this is an autotag rule created by crossplane"
   # rules_maintained_externally = false
   rules {
@@ -17,32 +17,6 @@ resource "dynatrace_autotag_v2" "crossplane-created" {
             key            = "HOST_GROUP_NAME"
             operator       = "EQUALS"
             string_value   = "fooCrossplane"
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "dynatrace_autotag_v2" "manually-created" {
-  name                          = "manually-created"
-  description                   = "this is a manually created autotag rule"
-  # rules_maintained_externally = false
-  rules {
-    rule {
-      type                = "ME"
-      enabled             = true
-      value_normalization = "Leave text as-is"
-      attribute_rule {
-        entity_type                 = "SERVICE"
-        service_to_host_propagation = true
-        service_to_pgpropagation    = true
-        conditions {
-          condition {
-            case_sensitive = true
-            key            = "HOST_GROUP_NAME"
-            operator       = "EQUALS"
-            string_value   = "foo"
           }
         }
       }
