ee608d72d8708f95e2d808594627c269428422d4
Adam Gardner <26523841+agardnerIT@users.noreply.github.com>
2024-09-28 20:15:18 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,3 +23,29 @@ resource "dynatrace_autotag_v2" "crossplane-created" {
     }
   }
 }
+
+resource "dynatrace_autotag_v2" "manually-created" {
+  name                          = "manually-created"
+  description                   = "this is a manually created autotag rule"
+  # rules_maintained_externally = false
+  rules {
+    rule {
+      type                = "ME"
+      enabled             = true
+      value_normalization = "Leave text as-is"
+      attribute_rule {
+        entity_type                 = "SERVICE"
+        service_to_host_propagation = true
+        service_to_pgpropagation    = true
+        conditions {
+          condition {
+            case_sensitive = true
+            key            = "HOST_GROUP_NAME"
+            operator       = "EQUALS"
+            string_value   = "foo"
+          }
+        }
+      }
+    }
+  }
+}
